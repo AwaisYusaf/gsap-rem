402ed68d88ac99dd0bc3016a6036d0a65767750c
AwaisYusaf <muhammadawais6640@gmail.com>
2023-09-30 21:40:06 +0500

    Bottom bar v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <ScrollAnimationSection />
       <div className='flex justify-center space-x-2 mx-auto max-w-xl mb-4 px-4'>
         <input placeholder='Enter your email'
-          className='basis-7/12 lg:py-4 py-3 px-5 text-base outline-none border border-black rounded-lg focus-within:ring-1 ring-[#8E33FF]' />
+          className='lg:basis-7/12 basis-1/2 lg:py-4 py-3 px-5 text-base outline-none border border-black rounded-lg focus-within:ring-1 ring-[#8E33FF]' />
         <button
           className='flex justify-center items-center space-x-2 text-white basis-1/2 rounded-lg lg:py-4 py-3 hover:opacity-90 bg-[#8E33FF]'>
           <span className='lg:text-[22px] md:text-xl text-base'>Join Waitlist</span>
